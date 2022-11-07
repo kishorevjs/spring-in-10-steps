c32fff1c92f71abd819872aa179d9d325bd38740
kishorevjs <jayasaikishore@gmail.com>
2022-11-06 16:57:55 -0800

    Implementing Basic Mock test and defining the dependencies in pom.xml

--- src/test/java/com/in28minutes/mockito/SomeBusinessImplTest.java
+++ src/test/java/com/in28minutes/mockito/SomeBusinessImplTest.java
@@ -1,23 +1,27 @@
 package com.in28minutes.mockito;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplTest {
+
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
 
     @Test
     void test(){
-        DataServiceStub dataServiceStub = new DataServiceStub();
-        SomeBusinessImpl businessImpl = new SomeBusinessImpl(dataServiceStub);
-        int result = businessImpl.findTheGreatestFromAllData();
-        assertEquals(26, result);
-    }
-}
 
-class DataServiceStub implements DataService{
-
-    @Override
-    public int[] retriveAllData() {
-        return new int[]{26, 10, 22};
+        when(dataServiceMock.retriveAllData()).thenReturn(new int[]{20, 20});
+        assertEquals(20, businessImpl.findTheGreatestFromAllData());
     }
 }
